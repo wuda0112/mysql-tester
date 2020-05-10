7853da4dc22c6145a9ea56f3fb796f4760aef4d7
wuda0112 <wuda0112@gmail.com>
2020-05-10 22:47:25 +0800

    为package-info编写更详细的注释

--- mysql-data-generator/src/main/java/com/wuda/tester/mysql/generate/package-info.java
+++ mysql-data-generator/src/main/java/com/wuda/tester/mysql/generate/package-info.java
@@ -1,5 +1,6 @@
 /**
- * 数据生成.
+ * 主要处理数据生成.{@link com.wuda.tester.mysql.generate.DataFactory}类用于为每个表生成Insert into ...Values语句,
+ * {@link com.wuda.tester.mysql.generate.DataGenerator}类用于组织生成数据的完整逻辑.
  *
  * @author wuda
  */
